64181b6d665c7c735d05ebf7eac0e580e9818b4d
NusDev <Peerapatlaelem001@gmail.com>
2026-02-19 16:15:20 +0700

    setup docker

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AntdRegistry>K{children}</AntdRegistry>
+        <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
   );
