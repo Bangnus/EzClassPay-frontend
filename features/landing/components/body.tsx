599e73bf63a39cb48be4b388ce469cb109ffe18a
NusDev <Peerapatlaelem001@gmail.com>
2026-06-06 07:39:52 +0700

    style(theme): add EzClassPay color palette

--- features/landing/components/body.tsx
+++ features/landing/components/body.tsx
@@ -1,6 +1,6 @@
 export default function Body() {
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 text-text-primary">
       <h1>Body</h1>
     </div>
   );
