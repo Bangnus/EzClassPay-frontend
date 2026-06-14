e0ab8bb552d56af69fa34eac987d77d4c1dc7bdb
NusDev <Peerapatlaelem001@gmail.com>
2026-06-15 00:34:22 +0700

    refactor: remove unused submitted state and improve error handling in CreateRoomForm

--- features/rooms/repository.ts
+++ features/rooms/repository.ts
@@ -1,16 +1,7 @@
-"use server";
-
 import { axiosInstance } from "@/lib/axios";
 import { CreateRoomPayload } from "./types";
 
 export const createRoomApi = async (payload: CreateRoomPayload) => {
-  try {
-    const response = await axiosInstance.post("/api/rooms", payload);
-    return response.data;
-  } catch (error: any) {
-    if (error.response && error.response.data) {
-      throw new Error(error.response.data.message || JSON.stringify(error.response.data));
-    }
-    throw error;
-  }
+  const response = await axiosInstance.post("/api/rooms", payload);
+  return response.data;
 };
